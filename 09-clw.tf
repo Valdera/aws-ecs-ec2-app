2af7884d2be76f57f0a5cee524276a2fb6c83d28
Valdera <f.valdera@yahoo.co.id>
2024-12-11 21:38:32 +0700

    remove unused vars

--- 09-clw.tf
+++ 09-clw.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "log_group_ecs" {
-  name              = "${local.cloudwatch_prefix}/ecs-${aws_ecs_cluster.ecs_cluster.id}"
+  name              = "${local.cloudwatch_prefix}/ecs"
   retention_in_days = var.log_retention_in_days
 
   tags = merge(local.common_tags,
